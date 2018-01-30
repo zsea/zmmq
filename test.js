407de192d3813e3438f856218142c606d49fa198
曾大海 <zsea@qq.com>
2018-01-30 17:22:38 +0800

    完成事务代码

--- test.js
+++ test.js
@@ -6,10 +6,13 @@ var mmq = require('./lib/index');
     await trans.Insert(mq.Model, new mq.Model({
         body: "abcdefg"
     }));
-    await trans.Insert(mq.Model, new mq.Model({
+    /*await trans.Insert(mq.Model, new mq.Model({
         body: "xxxxkkk"
-    }));
+    }));*/
+    await trans.findByIdAndUpdate(mq.Model, "5a702714a68a200130165086", { body: 'xxxdf' });
+    //await trans.findByIdAndRemove(mq.Model, "5a6fe761ac5ced10bc65b7cd");
     await trans.Commit();
+    //await trans.Rollback();
     console.log("completed.");
     process.exit();
 })();
