25b39c0870f3eb5250a5f525c1be2a5adab5bdcf
zsea.home <zsea@home>
2018-02-14 22:31:37 +0800

    修正push错误

--- test/push.js
+++ test/push.js
@@ -3,15 +3,16 @@ var sleep = require('../lib/sleep');
 (async function name(params) {
     var mq = new mmq("mongodb://localhost/mq", "test");
     //var trans = await mq.Transaction.Begin();
-    var trans = await mq.Begin();
-    for (let i = 0; i < 10; i++) {
+    //var trans = await mq.Begin();
+    //for (let i = 0; i < 10; i++) {
         //await trans.push(i,{tag:i<5?"海海":"蚊子"});
-        await trans.push("levle", { level: 10 });
+        //await trans.push("levle", { level: 10 });
         //await sleep(1000);
         //break;
-    }
+    //}
 
-    await trans.Commit();
+    //await trans.Commit();
+    await mq.push("push test");
     console.log("pushed.");
     process.exit();
 })();
