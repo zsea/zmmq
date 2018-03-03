6a3d896ee62a05b26b3dfeb4bc9a86c6bdaf477d
zsea.home <zsea@home>
2018-03-03 14:35:50 +0800

    修复子队列完成时不在父队列中清除相关状态的bug

--- test/child.done.js
+++ test/child.done.js
@@ -1,7 +1,14 @@
 var mmq = require('../lib/index');
 var sleep = require('../lib/sleep');
+process.on('unhandledRejection', function (reason, p) {
+    console.error("Promise中有未处理的错误", p, " 错误原因: ", reason);
+    // application specific logging, throwing an error, or other logic here
+    setTimeout(function () {
+        process.exit(1);
+    }, 5000)
+});
 (async function name(params) {
-    var mq = new mmq("mongodb://localhost/mq", "children");
+    var mq = new mmq("mongodb://localhost/mq", "children", { done_mode: "DEL" });
     while (true) {
         var msg = await mq.pull();
         console.log("child", msg);
--- test/father.push.js
+++ test/father.push.js
@@ -1,13 +1,13 @@
 var mmq = require('../lib/index');
 var sleep = require('../lib/sleep');
-(async function() {
+(async function () {
     var mq = new mmq("mongodb://localhost/mq", "children");
     //var trans = await mq.Transaction.Begin();
     var trans = await mq.Begin();
     var fid = await trans.push("father_msg", null, 'father')
     for (let i = 0; i < 10; i++) {
         //await trans.push(i,{tag:i<5?"海海":"蚊子"});
-        let mid = await trans.push("levle", { level: 10, father: "father:" + fid },"children");
+        let mid = await trans.push("msg" + i, { level: 10, father: "father:" + fid }, "children");
         //ids.push();
         //await sleep(1000);
         //break;
